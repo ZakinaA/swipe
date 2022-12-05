01f327d8a3fb9402c55fe8b135090d8921bc6d20
theolevr <teolelievre2610@gmail.com>
2022-12-05 10:53:06 +0100

    consulter cours

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -25,4 +25,19 @@ class CoursController extends AbstractController
         return $this->render('cours/lister.html.twig', [
             'pCours' => $cours,]);	
     }
+
+    public function consulterCours(ManagerRegistry $doctrine, int $id){
+		
+
+        $cours = $doctrine->getRepository(Cours::class)->find($id);
+    
+        if (!$cours) {
+            throw $this->createNotFoundException(
+            'Aucun cours trouvé avec le numéro '.$id
+            );
+        }
+    
+        return $this->render('cours/consulter.html.twig', [
+            'cours' => $cours,]);
+    }
 }
