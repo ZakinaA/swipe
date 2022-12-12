57dfa40694cb9e7ff136594e1dea08c2a1214033
SovannThomas <thomasovann@gmail.com>
2022-12-12 16:44:42 +0100

    ETQ responsable, je peux ajouter un élève dont je serais responsable

---
+++ src/Form/EleveType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Compte;
+use App\Entity\Eleve;
+use App\Entity\Tranche;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class EleveType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add('nom',TextType::class)
+            ->add('prenom',TextType::class)
+
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Eleve::class,
+        ]);
+    }
+}
