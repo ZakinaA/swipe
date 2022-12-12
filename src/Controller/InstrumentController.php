3b1ee4741c973288ce56c22e1bcfb86a3b0efd19
theolevr <teolelievre2610@gmail.com>
2022-12-12 10:58:06 +0100

    Ajout de la fonction supprimer un instrument

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -11,6 +11,9 @@ use App\Entity\ClasseInstrument;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\InstrumentType;
+use App\Entity\Accessoire;
+use App\Entity\Intervenir;
+use App\Entity\Emprunter;
 
 class InstrumentController extends AbstractController
 {
@@ -98,9 +101,9 @@ public function instrumentListerTypeByID(ManagerRegistry $doctrine, int $id){
 
 public function ajouterInstrument(ManagerRegistry $doctrine,Request $request){
     $instrument = new instrument();
-$form = $this->createForm(InstrumentType::class, $instrument);
-$form->handleRequest($request);
-if ($form->isSubmitted() && $form->isValid()) {
+    $form = $this->createForm(InstrumentType::class, $instrument);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
 
         $instrument = $form->getData();
 
@@ -109,10 +112,46 @@ if ($form->isSubmitted() && $form->isValid()) {
         $entityManager->flush();
 
     return $this->render('instrument/consulter.html.twig', ['instrument' => $instrument,]);
-}
-else
-    {
+        }
+    else
+       {
         return $this->render('instrument/ajouter.html.twig', array('form' => $form->createView(),));
 }
+}
+
+public function supprimerInstrument(ManagerRegistry $doctrine, int $id, Request $request){
+
+    $accessoire = $doctrine->getRepository(Accessoire::class)->findByInstrument($id);
+    $intervenir = $doctrine->getRepository(Intervenir::class)->findByInstrument($id);
+    $emprunter = $doctrine->getRepository(Emprunter::class)->findByInstrument($id);
+    $instrument = $doctrine->getRepository(Instrument::class)->find($id);
+
+    if (!$instrument){
+        throw $this->createNotFoundException('Aucun instrument trouvé avec cet id !');
+    }
+    else{
+        $entityManager = $doctrine->getManager();
+        if ($accessoire){
+            for($i=0;$i<count($accessoire);$i++){
+                $entityManager->remove($accessoire[$i]);
+                $entityManager->flush();
+            }
+        }
+        if ($intervenir){
+            for($i=0;$i<count($intervenir);$i++){
+                $entityManager->remove($intervenir[$i]);
+                $entityManager->flush();
+            }
+        }
+        if ($emprunter){
+            for($i=0;$i<count($emprunter);$i++){
+                $entityManager->remove($emprunter[$i]);
+                $entityManager->flush();
+            }
+        }
+        $entityManager->remove($instrument);
+        $entityManager->flush();
+        return $this->redirectToRoute('instrumentListerType');
+    }
 }
 }
